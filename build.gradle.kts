a35904e61c7ef9f1bb71aa7cecbad7288f981b73
Hleb Ramniou <hramniou@eurofunk.com>
2025-05-29 12:46:00 +0200

    FOUND-2760 Adapted build.gradle.kts for publishing to GitHub Packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,16 @@ gradlePlugin {
 publishing {
     repositories {
         mavenLocal()
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/eurofunk/sbom-license-plugin")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
+            }
+        }
     }
 }
 
